133929eb05511427571ab57ab256b474b6bfa582
Nikch22 <65140836+Nikch22@users.noreply.github.com>
2021-05-27 09:41:48 -0500

    Add files via upload
    Commit prueba servicio cammel

---
+++ src/main/java/co/gov/jep/integracion/constante/UtilConstante.java
@@ -0,0 +1,12 @@
+package co.gov.jep.integracion.constante;
+
+public class UtilConstante {
+	
+	public static final String StatusCodeOk = "M0000";
+	public static final String StatusCodeCampoInvalido = "M0001";
+	public static final String StatusCodeErrorTecnico = "M0003";
+	public static final String StatusCodeUnauthorized = "M0008";
+	
+	
+	
+}
